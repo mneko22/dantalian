de43ec140392575989cbcd1942a264f5412f5f0d
mneko22 <c01144737e@edu.teu.ac.jp>
2023-10-22 01:43:14 +0900

    formatting

--- bookshelf.ts
+++ bookshelf.ts
@@ -1,133 +1,151 @@
 import { ulid } from "https://deno.land/x/ulid@v0.3.0/mod.ts";
 
 interface Book {
-    title: string
-    author?: string
-    url?: string
-    coverURL?: string
-    tags: Tag[]
-    status: string
+  title: string;
+  author?: string;
+  url?: string;
+  coverURL?: string;
+  tags: Tag[];
+  status: string;
 }
 
 interface iBookshelfRepository {
-    getBooks(): Promise<Book[]>
-    getTags(): Promise<Tag[]>
-    getBooksByTag(tag: Tag): Promise<Book[]>
-    getBook(id: string): Promise<Book|null>
-    addBook(book: Book): Promise<boolean>
-    updateBook(id: string, book: Book): Promise<boolean>
-    removeBook(id: string): Promise<boolean>
+  getBooks(): Promise<Book[]>;
+  getTags(): Promise<Tag[]>;
+  getBooksByTag(tag: Tag): Promise<Book[]>;
+  getBook(id: string): Promise<Book | null>;
+  addBook(book: Book): Promise<boolean>;
+  updateBook(id: string, book: Book): Promise<boolean>;
+  removeBook(id: string): Promise<boolean>;
 }
 
-type Tag = string
+type Tag = string;
 
 export class BookshelService {
-    shelfRepo: iBookshelfRepository
-    constructor(repo: iBookshelfRepository) {
-        this.shelfRepo = repo
+  shelfRepo: iBookshelfRepository;
+  constructor(repo: iBookshelfRepository) {
+    this.shelfRepo = repo;
+  }
+
+  async add(): Promise<void> {
+    const title = prompt("title? ->");
+    if (title == null) {
+      alert("Please enter a title");
+      return;
     }
-    
-    async add(): Promise<void> {
-        const title = prompt("title? ->")
-        if (title == null) {
-            alert("Please enter a title")
-            return
-        }
-        const author = prompt("auther? ->")
-        const url = prompt("url? ->")
-        const coverURL = prompt("cover url? ->")
-        const tagStr = prompt("tag? ->")
-        const tags = tagStr?.split("")
-        const status = prompt("status? ->")
-        const book: Book = {
-            title: title,
-            author: author ? author:undefined,
-            url: url ? url : undefined,
-            coverURL: coverURL ? coverURL : undefined,
-            tags: tags?tags:[],
-            status: status?status:"stack"
-        }
-        if (await this.shelfRepo.addBook(book)) {
-            alert("success")
-        }
+    const author = prompt("auther? ->");
+    const url = prompt("url? ->");
+    const coverURL = prompt("cover url? ->");
+    const tagStr = prompt("tag? ->");
+    const tags = tagStr?.split("");
+    const status = prompt("status? ->");
+    const book: Book = {
+      title: title,
+      author: author ? author : undefined,
+      url: url ? url : undefined,
+      coverURL: coverURL ? coverURL : undefined,
+      tags: tags ? tags : [],
+      status: status ? status : "stack",
+    };
+    if (await this.shelfRepo.addBook(book)) {
+      alert("success");
     }
-    async getBookList(): Promise<void> {
-        const books = await this.shelfRepo.getBooks()
-        for(const book of books){
-            console.log("title: ", book.title, " author: ", book.author, " url: ", book.url, " status: ", book.status)
-        }
+  }
+  async getBookList(): Promise<void> {
+    const books = await this.shelfRepo.getBooks();
+    for (const book of books) {
+      console.log(
+        "title: ",
+        book.title,
+        " author: ",
+        book.author,
+        " url: ",
+        book.url,
+        " status: ",
+        book.status,
+      );
     }
-    async getTagList(): Promise<void> {
-        const tags = await this.shelfRepo.getTags()
-        for(const tag of tags) {
-            console.log(tag)
-        }
+  }
+  async getTagList(): Promise<void> {
+    const tags = await this.shelfRepo.getTags();
+    for (const tag of tags) {
+      console.log(tag);
     }
-    async getBookListByTag(): Promise<void> {
-        const tag = prompt("tag ->")
-        if (tag == null) {
-            return
-        }
-        const books = await this.shelfRepo.getBooksByTag(tag)
-        for(const book of books){
-            console.log("title: ", book.title, " author: ", book.author, " url: ", book.url, " status: ", book.status)
-        }
+  }
+  async getBookListByTag(): Promise<void> {
+    const tag = prompt("tag ->");
+    if (tag == null) {
+      return;
     }
+    const books = await this.shelfRepo.getBooksByTag(tag);
+    for (const book of books) {
+      console.log(
+        "title: ",
+        book.title,
+        " author: ",
+        book.author,
+        " url: ",
+        book.url,
+        " status: ",
+        book.status,
+      );
+    }
+  }
 }
 
 export class BookshelfRepo implements iBookshelfRepository {
-    kv: Deno.Kv
-    constructor(kv: Deno.Kv) {
-        this.kv = kv
-    }
+  kv: Deno.Kv;
+  constructor(kv: Deno.Kv) {
+    this.kv = kv;
+  }
 
-    async getBooks(): Promise<Book[]> {
-        const it = this.kv.list<Book>({ prefix: ["books"] })
-        const books: Book[] = []
-        for await (const res of it) books.push(res.value)
-        return books
-    }
+  async getBooks(): Promise<Book[]> {
+    const it = this.kv.list<Book>({ prefix: ["books"] });
+    const books: Book[] = [];
+    for await (const res of it) books.push(res.value);
+    return books;
+  }
 
-    async getTags(): Promise<Tag[]> {
-        const it = this.kv.list<Tag>({prefix: ["tags"]})
-        const tags: Tag[] = []
-        for await (const res of it) tags.push(res.value)
-        return tags
-    }
+  async getTags(): Promise<Tag[]> {
+    const it = this.kv.list<Tag>({ prefix: ["tags"] });
+    const tags: Tag[] = [];
+    for await (const res of it) tags.push(res.value);
+    return tags;
+  }
 
-    async getBooksByTag(tag: Tag): Promise<Book[]> {
-        const it = this.kv.list<Book>({ prefix: ["tags",tag] })
-        const books: Book[] = []
-        for await (const res of it) books.push(res.value)
-        return books
-    }
+  async getBooksByTag(tag: Tag): Promise<Book[]> {
+    const it = this.kv.list<Book>({ prefix: ["tags", tag] });
+    const books: Book[] = [];
+    for await (const res of it) books.push(res.value);
+    return books;
+  }
 
-    async getBook(id: string): Promise<Book|null> {
-        const result = await this.kv.get<Book|null>(["books", id])
-        return result.value
-    }
+  async getBook(id: string): Promise<Book | null> {
+    const result = await this.kv.get<Book | null>(["books", id]);
+    return result.value;
+  }
 
-    async addBook(book: Book): Promise<boolean> {
-        const id = ulid()
-        const tx = this.kv.atomic().set(["books", id], book)
-        for (const t of book.tags) {
-            tx.set(["tags", t, id], book)
-        }
-        const result = await tx.commit()
-        return result.ok
+  async addBook(book: Book): Promise<boolean> {
+    const id = ulid();
+    const tx = this.kv.atomic().set(["books", id], book);
+    for (const t of book.tags) {
+      tx.set(["tags", t, id], book);
     }
+    const result = await tx.commit();
+    return result.ok;
+  }
 
-    async updateBook(id: string, book: Book): Promise<boolean> {
-        const tx = this.kv.atomic().set(["books", id], book)
-        for(const t of book.tags) {
-            tx.set(["tags", t, id], book)
-        }
-        const result = await tx.commit()
-        return result.ok
+  async updateBook(id: string, book: Book): Promise<boolean> {
+    const tx = this.kv.atomic().set(["books", id], book);
+    for (const t of book.tags) {
+      tx.set(["tags", t, id], book);
     }
+    const result = await tx.commit();
+    return result.ok;
+  }
 
-    async removeBook(id: string): Promise<boolean> {
-        const _res = await this.kv.delete(["books", id])
-        return true
-    }
+  async removeBook(id: string): Promise<boolean> {
+    const _res = await this.kv.delete(["books", id]);
+    return true;
+  }
 }
--- main.ts
+++ main.ts
@@ -1,24 +1,23 @@
-import { parse } from "https://deno.land/std@0.204.0/flags/mod.ts"
-import {BookshelfRepo, BookshelService} from "./bookshelf.ts"
+import { parse } from "https://deno.land/std@0.204.0/flags/mod.ts";
+import { BookshelfRepo, BookshelService } from "./bookshelf.ts";
 
 function main(args: string[], service: BookshelService): Promise<void> {
-  const parsedArgs = parse(args)
-  switch(parsedArgs._[0]) {
+  const parsedArgs = parse(args);
+  switch (parsedArgs._[0]) {
     case "add":
-      return service.add()
+      return service.add();
     case "ls":
-      return service.getBookList()
-    case "tags": 
-      return service.getBookListByTag()
+      return service.getBookList();
+    case "tags":
+      return service.getBookListByTag();
     default:
-      return service.getBookList()
+      return service.getBookList();
   }
 }
 
-
 if (import.meta.main) {
-  const kv = await Deno.openKv()
-  const rep = new BookshelfRepo(kv)
-  const sv = new BookshelService(rep)
-  await main(Deno.args, sv)
+  const kv = await Deno.openKv();
+  const rep = new BookshelfRepo(kv);
+  const sv = new BookshelService(rep);
+  await main(Deno.args, sv);
 }
